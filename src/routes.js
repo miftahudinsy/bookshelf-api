878c7063a063628ba65303b00ca670080efd48da
miftahudin <miftahudin.syarif@gmail.com>
2025-05-18 00:20:38 +0700

    feat: add editBookByIdHandler

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import {
   addBookHandler,
   getAllBooksHandler,
   getBookByIdHandler,
+  editBookByIdHandler,
 } from "./handler.js";
 
 const routes = [
@@ -19,6 +20,11 @@ const routes = [
     method: "GET",
     path: "/books/{id}",
     handler: getBookByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/books/{id}",
+    handler: editBookByIdHandler,
   },
 ];
 
