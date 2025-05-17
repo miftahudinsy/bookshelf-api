f9a000cb487d867c5878617001097740a588e841
miftahudin <miftahudin.syarif@gmail.com>
2025-05-18 00:14:47 +0700

    feat: add getBookByIdHandler

--- src/handler.js
+++ src/handler.js
@@ -83,4 +83,25 @@ const getAllBooksHandler = (_, h) => {
   return response;
 };
 
-export { addBookHandler, getAllBooksHandler };
+const getBookByIdHandler = (request, h) => {
+  const { id } = request.params;
+  const book = books.filter((n) => n.id === id)[0];
+
+  if (book !== undefined) {
+    return {
+      status: "success",
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: "fail",
+    message: "Buku tidak ditemukan",
+  });
+  response.code(404);
+  return response;
+};
+
+export { addBookHandler, getAllBooksHandler, getBookByIdHandler };
